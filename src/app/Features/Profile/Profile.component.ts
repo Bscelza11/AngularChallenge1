9f2fe26845196faad16e45ab61b64a29333bebe9
Barbara Scelza <barbara.scelza@dualbootpartners.com>
2023-01-24 17:42:57 -0300

    add greetings

--- src/app/Features/Profile/Profile.component.ts
+++ src/app/Features/Profile/Profile.component.ts
@@ -8,7 +8,9 @@ import { authenticationService } from 'src/app/Core/Authentication.service';
 })
 export class ProfileComponent implements OnInit, OnDestroy {
   name: any;
-
+  greetings = ['Hola', 'Buen dia', 'Eres genial'];
+  index = Math.floor(Math.random() * this.greetings.length);
+  greeting = this.greetings[this.index];
   constructor(private authenticationService: authenticationService) {}
 
   ngOnInit(): void {
