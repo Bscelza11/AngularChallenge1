b3933af85e59cb105408cccd3966fa3a59a86776
Bscelza11 <bscelza11@gmail.com>
2023-01-31 17:16:05 -0300

    delete message profile

--- src/app/Features/Profile/Profile.component.ts
+++ src/app/Features/Profile/Profile.component.ts
@@ -8,9 +8,7 @@ import { authenticationService } from 'src/app/Core/authentication.service';
 })
 export class ProfileComponent implements OnInit, OnDestroy {
   name: any;
-  greetings = ['Hola', 'Buen dia', 'Eres genial'];
-  index = Math.floor(Math.random() * this.greetings.length);
-  greeting = this.greetings[this.index];
+
   constructor(private authenticationService: authenticationService) {}
 
   ngOnInit(): void {
